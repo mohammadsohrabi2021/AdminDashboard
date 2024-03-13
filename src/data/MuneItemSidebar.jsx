6525c7cc6ad35a975aca98f0e66c21e7446aa730
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-13 20:01:24 +0330

    create muneItemSideBar data done

---
+++ src/data/MuneItemSidebar.jsx
@@ -0,0 +1,42 @@
+const muneItemSideBar = [
+  {
+    id: 1,
+    title: "quickLinks",
+    mune: [
+      { id: 2, name: "dashboard",href:"/", icon: "" },
+      { id: 3, name: "newProduct",href:"/newProduct", icon: "" },
+      { id: 4, name: "newCoupon",href:"/newCoupon", icon: "" },
+    ],
+  },
+  {
+    id: 5,
+    title: "catalog",
+    mune: [
+      { id: 6, name: "products",href:"/products", icon: "" },
+      { id: 7, name: "categories",href:"/categories", icon: "" },
+      { id: 8, name: "collections",href:"/collections", icon: "" },
+      { id: 9, name: "attributes",href:"/attributes", icon: "" },
+    ],
+  },
+  {
+    id: 10,
+    title: "sale",
+    mune: [{ id: 11, name: "orders",href:"/orders", icon: "" }],
+  },
+  {
+    id: 12,
+    title: "customers",
+    mune: [{ id: 13, name: "customers",href:"/customers", icon: "" }],
+  },
+  {
+    id: 14,
+    title: "promotion",
+    mune: [{ id: 15, name: "coupons",href:"/coupons", icon: "" }],
+  },
+  {
+    id: 16,
+    title: "cms",
+    mune: [{ id: 17, name: "pages",href:"/pages", icon: "" }],
+  },
+];
+export default muneItemSideBar;
