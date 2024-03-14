9f530e7acd3933acf3dd2ceb271b15da76c42e14
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-14 19:34:51 +0330

    update router file done

--- src/router.jsx
+++ src/router.jsx
@@ -2,32 +2,42 @@ import { createBrowserRouter } from "react-router-dom";
 import Login, { loginAction } from "./features/identity/components/Login";
 import IdentityLayout from "./layouts/identity-layout";
 import MainLayout from "./layouts/mainLayout/MainLayout";
+import Home from "./pages/Home";
+import NewCoupon from "./pages/NewCoupon";
+import NewProduct from "./pages/NewProduct";
+import Products from "./pages/Products";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     // errorElement: <UnhandlesException />,
-    // children: [
-    //   {
-    //     element: <Courses />,
-    //     index: true,
-    //     loader: coursesLoader,
-    //   },
-    //   {
-    //     path: "course-categories",
-    //     element: (
-    //       <CategoryProvider>
-    //         <CourseCategories />
-    //       </CategoryProvider>
-    //     ),
-    //     loader: categoriesLoader,
-    //   },
-    //   {
-    //     path: "courses/:id",
-    //     element: <CourseDetailes />,
-    //     loader: courseDetailsLoader,
-    //   },
-    // ],
+    children: [
+      {
+        element: <Home />,
+        index: true,
+        // loader: coursesLoader,
+      },
+      {
+        path: "newProduct",
+        element: <NewProduct />,
+        // loader: categoriesLoader,
+      },
+      {
+        path: "products",
+        element: <Products />,
+        // loader: categoriesLoader,
+      },
+      {
+        path: "newCoupon",
+        element: <NewCoupon />,
+        // loader: categoriesLoader,
+      },
+      // {
+      //   path: "courses/:id",
+      //   element: <CourseDetailes />,
+      //   loader: courseDetailsLoader,
+      // },
+    ],
   },
   {
     element: <IdentityLayout />,
