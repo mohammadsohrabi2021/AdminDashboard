7541372dff4df8290fd6da076e19b4ddbd717aee
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-21 15:18:03 +0330

    refactor component Products done

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { httpInterceptedServices } from '@core/http-service'
+import { Await, useLoaderData } from 'react-router-dom';
+import ProductList from '../features/products/components/ProductList';
 
-function Products() {
+const Products=()=> {
+  const data = useLoaderData();
   return (
-    <div>Products</div>
+    <div className="row">
+    <div className="col-12">
+      <div className="d-flex align-items-center justify-content-between m-5">
+        <a href="" className="btn btn-primary fw-bolder mt-n1">
+          افزودن محصول جدید
+        </a>
+      </div>
+      <Suspense
+        fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
+      >
+        <Await resolve={data.courses}>
+          {(loadedCourses) => <ProductList products={loadedCourses} />}
+        </Await>
+      </Suspense>
+    </div>
+  </div>
   )
 }
-
+export  async function productsLoader(){
+  const response =await httpInterceptedServices.get('/products/all/items/1/page/1')
+  console.log(response)
+  return response.data
+}
 export default Products
