354ae73308e6fff447c195a4fca22e71d7330f12
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-21 15:24:26 +0330

    add coment loject home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+// import { httpInterceptedServices } from '../core/http-service'
 
 function Home() {
   return (
     <div>Home</div>
   )
 }
-
+// export async function homeLoader(){
+//   const response =await httpInterceptedServices.get()
+// }
 export default Home
