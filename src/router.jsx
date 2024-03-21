48900386247d0ad2c55c0785f80ed954463180a2
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-21 15:23:53 +0330

    refactor file router done

--- src/router.jsx
+++ src/router.jsx
@@ -1,21 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import { CategoryProvider } from "./features/categories/components/CategoryContext";
 import Login, { loginAction } from "./features/identity/components/Login";
+import ProductsDetails, { productsDetailsLoader } from "./features/products/components/ProductsDetails";
 import IdentityLayout from "./layouts/identity-layout";
 import MainLayout from "./layouts/mainLayout/MainLayout";
+import CategoriesProduct, { categoriesLoader } from "./pages/CategoriesProduct";
 import Home from "./pages/Home";
 import NewCoupon from "./pages/NewCoupon";
 import NewProduct from "./pages/NewProduct";
-import Products from "./pages/Products";
+import Products, { productsLoader } from "./pages/Products";
+import UnhandlesException from "./pages/UnhandlesException";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    // errorElement: <UnhandlesException />,
+    errorElement: <UnhandlesException />,
     children: [
       {
         element: <Home />,
         index: true,
-        // loader: coursesLoader,
+        // loader: homeLoader,
       },
       {
         path: "newProduct",
@@ -25,12 +29,26 @@ const router = createBrowserRouter([
       {
         path: "products",
         element: <Products />,
-        // loader: categoriesLoader,
+        loader: productsLoader,
+      },
+      {
+        path: "products/:id",
+        element: <ProductsDetails />,
+        loader: productsDetailsLoader,
       },
       {
         path: "newCoupon",
         element: <NewCoupon />,
         // loader: categoriesLoader,
+      },
+      {
+        path: "categories",
+        element: (
+          <CategoryProvider>
+            <CategoriesProduct />
+          </CategoryProvider>
+        ),
+        loader: categoriesLoader,
       },
       // {
       //   path: "courses/:id",
