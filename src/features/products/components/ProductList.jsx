8f8f9c88c2fc0da142aa615a5b0770dbf48b5ca7
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2024-03-25 03:09:59 +0330

    update component ProductList done

--- src/features/products/components/ProductList.jsx
+++ src/features/products/components/ProductList.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Pagination from "../../../components/Pagination";
+import Product from "./Product";
+import { Card, Grid } from "@mui/material";
+import { httpInterceptedServices } from "@core/http-service";
 
-function ProductList({products}) {
+function ProductList({ products, deleteProduct }) {
+  const [totalCount, setTotalCount] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchTotalContProduct = async () => {
+      const response = await httpInterceptedServices.get(
+        '/products/get/total_count/'
+      );
+      setTotalCount(response?.data?.total_count);
+    };
+    fetchTotalContProduct();
+  }, [location]); // useEffect را با تغییر مکان فعلی فراخوانی کنید
+
   return (
-    <>
-      <div className="row">
-        {products.map((product) => (
-          <div className="col-12 col-md-6 col-lg-3" key={product.id}>
-            <Course {...product} />
-          </div>
+    <Grid>
+      <Grid display={"flex"} flexWrap={"wrap"} gap={2}>
+        {products.map((item) => (
+          <Card zIndex={-1} key={item._id}>
+            <Product {...item} deleteProduct={deleteProduct} />
+          </Card>
         ))}
-      </div>
-    </>
-  )
+      </Grid>
+      <Grid className="card-footer" mt={2}>
+        <Pagination totalRecords={totalCount} />
+      </Grid>
+    </Grid>
+  );
 }
 
-export default ProductList
+export default ProductList;
